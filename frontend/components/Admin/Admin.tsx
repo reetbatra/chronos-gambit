b5d7d20185d75069ea2e54668eac16e8bce4c26f
sharayu <sharayu@cooeyhealth.com>
2024-08-29 22:06:48 +0530

    added vercel

--- frontend/components/Admin/Admin.tsx
+++ frontend/components/Admin/Admin.tsx
@@ -1,6 +1,5 @@
 import {Box, Center} from '@chakra-ui/react'
 import AdminForm from './AdminForm'
-import VoteTable from './VoteTable'
 
 type Props = {}
 
@@ -20,14 +19,14 @@ function Admin({}: Props) {
                     <AdminForm/>
 
         </Center>
-        <Box my ={20} mx={10}>  
+        {/* <Box my ={20} mx={10}>  
         <Center>
                 <h1 className='font-jbm text-white font-bold text-2xl'>Approve data from oracles</h1>
 
         </Center>      
             <VoteTable/>
         </Box>
-        
+         */}
     </Box>
     </>
   )
